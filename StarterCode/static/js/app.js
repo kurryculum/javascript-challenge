8634acddb78c138add96b929fcdc43cf7e7c2e26
vineet <vineetsikri@yahoo.com>
2020-10-25 19:12:21 -0500

    added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,24 @@
-// from data.js
-var tableData = data;
+var tabledata = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody")
+
+data.forEach(function(data){
+
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+ var button = d3.select("#filter-btn");
+ var form = d3.select("#form");
+ button.on("click",runEnter);
+ button.on("submit",runEnter);
+ function runEnter(){
+ d3.event.preventDefault();
+ var inputElement = d3.select("datetime");
+ var inputValue = inputElement.property("value");
+
+ console.log(inputValue);
+ }
